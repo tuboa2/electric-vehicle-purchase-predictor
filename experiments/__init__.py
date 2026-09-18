811f2ca697f3031cb5afe917771648695da9b6c3
tuboa2 <ianjames.calumpang@gmail.com>
2026-09-18 16:09:10 +0800

    chore: absolute cleanup of all scaffolding and garbage files

--- experiments/__init__.py
+++
@@ -1,5 +0,0 @@
-from experiments.worktree import GitWorktreeManager
-from experiments.registry import RunRegistry
-from experiments.runner import ExperimentRunner
-
-__all__ = ["GitWorktreeManager", "RunRegistry", "ExperimentRunner"]
--- kaggle/__init__.py
+++
@@ -1,5 +0,0 @@
-from kaggle.hardware import HardwareManager
-from kaggle.kernel_builder import KernelBuilder
-from kaggle.client import KaggleClient
-
-__all__ = ["HardwareManager", "KernelBuilder", "KaggleClient"]
